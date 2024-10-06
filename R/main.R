6d25b5da641c7de6f5e3dbe5d88e0d99e60e0ea3
Hiroaki Yutani <yutani.ini@gmail.com>
2024-10-06 13:50:19 +0900

    Discard stdout

--- R/main.R
+++ R/main.R
@@ -4,7 +4,7 @@ vellogd_env <- new.env(parent = emptyenv())
 vellogd <- function(filename = "Rplot%03d.png", width = 480, height = 480) {
   cmd <- "./src/dep/target/release/vellogd-server.exe"
   args <- c(as.character(width), as.character(height))
-  vellogd_env$process <- processx::process$new(cmd, args, stdout = "|")
+  vellogd_env$process <- processx::process$new(cmd, args)
 
   vellogd_impl(filename, as.numeric(width), as.numeric(height))
 }
