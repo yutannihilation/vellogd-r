adf4c013329e53b7e444659f970325373d115247
Hiroaki Yutani <yutani.ini@gmail.com>
2024-10-16 11:10:53 +0900

    Tweak

--- src/rust/src/debug_device.rs
+++ src/rust/src/debug_device.rs
@@ -1,4 +1,3 @@
-use crate::graphics::ClippingStrategy;
 use crate::graphics::DevDesc;
 
 use crate::graphics::DeviceDriver;
@@ -26,59 +25,35 @@ fn text_related_params(gc: R_GE_gcontext) -> String {
 pub struct DebugGraphicsDevice {}
 
 impl DeviceDriver for DebugGraphicsDevice {
-    const USE_RASTER: bool = true;
-
-    const USE_CAPTURE: bool = true;
-
-    const USE_LOCATOR: bool = true;
-
-    const USE_PLOT_HISTORY: bool = false;
-
-    const CLIPPING_STRATEGY: ClippingStrategy = ClippingStrategy::DeviceAndEngine;
-
-    const ACCEPT_UTF8_TEXT: bool = true;
-
     fn activate(&mut self, _: DevDesc) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[activate]");
-        }
+        savvy::r_eprintln!("[activate]");
     }
 
     fn circle(&mut self, center: (f64, f64), r: f64, gc: R_GE_gcontext, _: DevDesc) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!(
-                "[circle] center: {center:?} r: {r}, fill params: {{ {} }}, line params: {{ {} }}",
-                fill_related_params(gc),
-                line_related_params(gc)
-            );
-        }
+        savvy::r_eprintln!(
+            "[circle] center: {center:?} r: {r}, fill params: {{ {} }}, line params: {{ {} }}",
+            fill_related_params(gc),
+            line_related_params(gc)
+        );
     }
 
     fn clip(&mut self, from: (f64, f64), to: (f64, f64), _: DevDesc) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[clip] from: {from:?}, to: {to:?}");
-        }
+        savvy::r_eprintln!("[clip] from: {from:?}, to: {to:?}");
     }
 
     fn close(&mut self, _: DevDesc) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[close]");
-        }
+        savvy::r_eprintln!("[close]");
     }
 
     fn deactivate(&mut self, _: DevDesc) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[deactivate]");
-        }
+        savvy::r_eprintln!("[deactivate]");
     }
 
     fn line(&mut self, from: (f64, f64), to: (f64, f64), gc: R_GE_gcontext, _: DevDesc) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!(
-                "[line] from: {from:?}, to: {to:?}, line params: {{ {} }}",
-                line_related_params(gc)
-            );
-        }
+        savvy::r_eprintln!(
+            "[line] from: {from:?}, to: {to:?}, line params: {{ {} }}",
+            line_related_params(gc)
+        );
     }
 
     fn char_metric(
@@ -87,12 +62,10 @@ impl DeviceDriver for DebugGraphicsDevice {
         gc: R_GE_gcontext,
         _: DevDesc,
     ) -> crate::graphics::TextMetric {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!(
-                "[char_metric] c: {c:?}, text params: {{ {} }}",
-                text_related_params(gc)
-            );
-        }
+        savvy::r_eprintln!(
+            "[char_metric] c: {c:?}, text params: {{ {} }}",
+            text_related_params(gc)
+        );
 
         crate::graphics::TextMetric {
             ascent: 0.0,
@@ -102,33 +75,23 @@ impl DeviceDriver for DebugGraphicsDevice {
     }
 
     fn mode(&mut self, mode: i32, _: DevDesc) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[mode] mode: {mode}");
-        }
+        savvy::r_eprintln!("[mode] mode: {mode}");
     }
 
     fn new_page(&mut self, gc: R_GE_gcontext, _: DevDesc) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[new_page]");
-        }
+        savvy::r_eprintln!("[new_page]");
     }
 
     fn polygon(&mut self, x: &[f64], y: &[f64], gc: R_GE_gcontext, _: DevDesc) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[polygon]");
-        }
+        savvy::r_eprintln!("[polygon]");
     }
 
     fn polyline(&mut self, x: &[f64], y: &[f64], gc: R_GE_gcontext, _: DevDesc) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[polyline]");
-        }
+        savvy::r_eprintln!("[polyline]");
     }
 
     fn rect(&mut self, from: (f64, f64), to: (f64, f64), gc: R_GE_gcontext, _: DevDesc) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[rect]");
-        }
+        savvy::r_eprintln!("[rect]");
     }
 
     fn path(
@@ -140,9 +103,7 @@ impl DeviceDriver for DebugGraphicsDevice {
         gc: R_GE_gcontext,
         dd: DevDesc,
     ) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[path] nper: {nper:?}");
-        }
+        savvy::r_eprintln!("[path] nper: {nper:?}");
     }
 
     fn raster<T: AsRef<[u32]>>(
@@ -155,29 +116,24 @@ impl DeviceDriver for DebugGraphicsDevice {
         gc: R_GE_gcontext,
         _: DevDesc,
     ) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[raster]");
-        }
+        savvy::r_eprintln!("[raster]");
     }
 
     fn capture(&mut self, _: DevDesc) -> savvy::ffi::SEXP {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[capture]");
-        }
+        savvy::r_eprintln!("[capture]");
+
         unsafe { R_NilValue }
     }
 
     fn size(&mut self, dd: DevDesc) -> (f64, f64, f64, f64) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[size]");
-        }
+        savvy::r_eprintln!("[size]");
+
         (dd.left, dd.right, dd.bottom, dd.top)
     }
 
     fn text_width(&mut self, text: &str, gc: R_GE_gcontext, dd: DevDesc) -> f64 {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[text_width]");
-        }
+        savvy::r_eprintln!("[text_width]");
+
         text.chars()
             .map(|c| self.char_metric(c, gc, dd).width)
             .sum()
@@ -192,41 +148,32 @@ impl DeviceDriver for DebugGraphicsDevice {
         gc: R_GE_gcontext,
         _: DevDesc,
     ) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[text] text: {text}");
-        }
+        savvy::r_eprintln!("[text] text: {text}");
     }
 
     fn on_exit(&mut self, _: DevDesc) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[on_exit]");
-        }
+        savvy::r_eprintln!("[on_exit]");
     }
 
     fn new_frame_confirm(&mut self, _: DevDesc) -> bool {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[new_frame_confirm]");
-        }
+        savvy::r_eprintln!("[new_frame_confirm]");
+
         true
     }
 
     fn holdflush(&mut self, _: DevDesc, level: i32) -> i32 {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[holdflush]");
-        }
+        savvy::r_eprintln!("[holdflush] level: {level}");
+
         0
     }
 
     fn locator(&mut self, x: *mut f64, y: *mut f64, _: DevDesc) -> bool {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[locator]");
-        }
+        savvy::r_eprintln!("[locator]");
+
         true
     }
 
     fn eventHelper(&mut self, _: DevDesc, code: i32) {
-        if cfg!(debug_assertions) {
-            savvy::r_eprintln!("[eventHelper] code {code}");
-        }
+        savvy::r_eprintln!("[eventHelper] code {code}");
     }
 }
