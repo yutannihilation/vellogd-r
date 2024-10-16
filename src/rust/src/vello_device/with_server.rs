2b311a99ff440fdbfca7007dc2823e12d70b64b4
Hiroaki Yutani <yutani.ini@gmail.com>
2024-10-17 00:15:41 +0900

    Update server

--- src/rust/src/vello_device/with_server.rs
+++ src/rust/src/vello_device/with_server.rs
@@ -98,47 +98,26 @@ impl DeviceDriver for VelloGraphicsDeviceWithServer {
         self.request_circle(center, r, gc).unwrap();
     }
 
-    fn clip(&mut self, from: (f64, f64), to: (f64, f64), _: DevDesc) {}
+    // TODO
+    // fn clip(&mut self, from: (f64, f64), to: (f64, f64), _: DevDesc) {}
 
     fn close(&mut self, _: DevDesc) {
         self.request_close_window().unwrap();
     }
 
-    fn deactivate(&mut self, _: DevDesc) {}
+    // TODO
+    // fn deactivate(&mut self, _: DevDesc) {}
 
     fn line(&mut self, from: (f64, f64), to: (f64, f64), gc: R_GE_gcontext, _: DevDesc) {
         self.request_line(from, to, gc).unwrap();
     }
 
     fn char_metric(&mut self, c: char, gc: R_GE_gcontext, _: DevDesc) -> TextMetric {
-        // TODO
-        let _family = unsafe {
-            CStr::from_ptr(gc.fontfamily.as_ptr())
-                .to_str()
-                .unwrap_or("Arial")
-        }
-        .to_string();
-        let size = gc.cex * gc.ps;
-        self.build_layout(c.to_string(), size as _, gc.lineheight as _);
-        let line = self.layout.lines().next();
-        match line {
-            Some(line) => {
-                let metrics = line.metrics();
-                TextMetric {
-                    ascent: metrics.ascent as _,
-                    descent: metrics.descent as _,
-                    width: self.layout.width() as _, // TOOD: should this be run.metrics().width of the first char?
-                }
-            }
-            None => TextMetric {
-                ascent: 0.0,
-                descent: 0.0,
-                width: 0.0,
-            },
-        }
+        self.get_char_metric(c, gc)
     }
 
-    fn mode(&mut self, mode: i32, _: DevDesc) {}
+    // TODO
+    // fn mode(&mut self, mode: i32, _: DevDesc) {}
 
     fn new_page(&mut self, _: R_GE_gcontext, _: DevDesc) {
         self.request_new_page().unwrap();
@@ -156,56 +135,43 @@ impl DeviceDriver for VelloGraphicsDeviceWithServer {
         self.request_rect(from, to, gc).unwrap();
     }
 
-    fn path(
-        &mut self,
-        x: &[f64],
-        y: &[f64],
-        nper: &[i32],
-        winding: bool,
-        gc: R_GE_gcontext,
-        dd: DevDesc,
-    ) {
-    }
+    // TODO
+    // fn path(
+    //     &mut self,
+    //     x: &[f64],
+    //     y: &[f64],
+    //     nper: &[i32],
+    //     winding: bool,
+    //     gc: R_GE_gcontext,
+    //     dd: DevDesc,
+    // ) {
+    // }
 
-    fn raster<T: AsRef<[u32]>>(
-        &mut self,
-        raster: crate::graphics::Raster<T>,
-        pos: (f64, f64),
-        size: (f64, f64),
-        angle: f64,
-        interpolate: bool,
-        gc: R_GE_gcontext,
-        _: DevDesc,
-    ) {
-    }
+    // TODO
+    // fn raster<T: AsRef<[u32]>>(
+    //     &mut self,
+    //     raster: crate::graphics::Raster<T>,
+    //     pos: (f64, f64),
+    //     size: (f64, f64),
+    //     angle: f64,
+    //     interpolate: bool,
+    //     gc: R_GE_gcontext,
+    //     _: DevDesc,
+    // ) {
+    // }
 
-    fn capture(&mut self, _: DevDesc) -> savvy::ffi::SEXP {
-        unsafe { R_NilValue }
-    }
+    // TODO
+    // fn capture(&mut self, _: DevDesc) -> savvy::ffi::SEXP {
+    //     unsafe { R_NilValue }
+    // }
 
     fn size(&mut self, dd: DevDesc) -> (f64, f64, f64, f64) {
         let sizes = self.get_window_sizes().unwrap_or((0, 0));
         (0.0, sizes.0 as _, 0.0, sizes.1 as _)
     }
 
-    fn text_width(&mut self, text: &str, gc: R_GE_gcontext, dd: DevDesc) -> f64 {
-        // TODO
-        let family = unsafe {
-            CStr::from_ptr(gc.fontfamily.as_ptr())
-                .to_str()
-                .unwrap_or("Arial")
-        }
-        .to_string();
-        // TODO
-        let _family = unsafe {
-            CStr::from_ptr(gc.fontfamily.as_ptr())
-                .to_str()
-                .unwrap_or("Arial")
-        }
-        .to_string();
-        let size = gc.cex * gc.ps;
-        self.build_layout(text, size as _, gc.lineheight as _);
-        self.layout.width() as _
+    fn text_width(&mut self, text: &str, gc: R_GE_gcontext, _: DevDesc) -> f64 {
+        self.get_text_width(text, gc)
     }
 
     fn text(
@@ -220,19 +186,24 @@ impl DeviceDriver for VelloGraphicsDeviceWithServer {
         self.request_text(pos, text, angle, hadj, gc).unwrap();
     }
 
-    fn on_exit(&mut self, _: DevDesc) {}
+    // TODO
+    // fn on_exit(&mut self, _: DevDesc) {}
 
-    fn new_frame_confirm(&mut self, _: DevDesc) -> bool {
-        true
-    }
+    // TODO
+    // fn new_frame_confirm(&mut self, _: DevDesc) -> bool {
+    //     true
+    // }
 
-    fn holdflush(&mut self, _: DevDesc, level: i32) -> i32 {
-        0
-    }
+    // TODO
+    // fn holdflush(&mut self, _: DevDesc, level: i32) -> i32 {
+    //     0
+    // }
 
-    fn locator(&mut self, x: *mut f64, y: *mut f64, _: DevDesc) -> bool {
-        true
-    }
+    // TODO
+    // fn locator(&mut self, x: *mut f64, y: *mut f64, _: DevDesc) -> bool {
+    //     true
+    // }
 
-    fn eventHelper(&mut self, _: DevDesc, code: i32) {}
+    // TODO
+    // fn eventHelper(&mut self, _: DevDesc, code: i32) {}
 }
